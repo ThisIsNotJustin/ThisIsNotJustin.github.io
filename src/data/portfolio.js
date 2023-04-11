3c0281408e5dced57f606f3b0c7332849e81e371
ThisIsNotJustin <JustinColemanOneTwo@gmail.com>
2023-04-11 02:11:14 -0500

    refactoring

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,37 +1,37 @@
 export default [
     {
         title: "Music Player (In Progress)",
-        imgUrl: "",
+        imgUrl: "public/assets/shazamAPI.jpeg",
         stack: ["JavaScript", "Shazam API"],
         link: "https://github.com/ThisIsNotJustin/Music-Player",
     },
     {
         title: "Portfolio Website",
-        imgUrl: "/assets/PortfolioWebsite.png",
+        imgUrl: "public/assets/PortfolioWebsite.png",
         stack: ["React", "Tailwind", "TypedJS", "Vite"],
         link: "https://github.com/ThisIsNotJustin/ThisIsNotJustin.github.io",
     },
     {
         title: "Facebook Web Scraper (In Progress)",
-        imgUrl: "/assets/BestBuy.png",
+        imgUrl: "public/assets/BestBuy.png",
         stack: ["Python", "Selenium", "Twilio API"],
         link: "https://github.com/ThisIsNotJustin/Facebook-Webscraper",
     },
     {
         title: "Retro Pokemon Game (In Progress)",
-        imgUrl: "/assets/SpaceGame.png",
+        imgUrl: "public/assets/SpaceGame.png",
         stack: ["HTML", "CSS", "JavaScript"],
         link: "https://github.com/ThisIsNotJustin/Retro-PokemonStyle-Game",
     },
     {
         title: "Study Timer",
-        imgUrl: "/assets/StudyTimer.png",
+        imgUrl: "public/assets/StudyTimer.png",
         stack: ["Java", "Java Swing"],
         link: "https://github.com/ThisIsNotJustin/StudyTimerGUI",
     },
     {
         title: "Hanoi Tower Solver",
-        imgUrl: "/assets/HanoiSolver.png",
+        imgUrl: "public/assets/HanoiSolver.png",
         stack: ["Java"],
         link: "https://github.com/ThisIsNotJustin/Hanoi-Tower-GameSimulation",
     },
